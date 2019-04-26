dc2779a5cea33ecd5d9e0997e382050380293b25
William Soukkachang <WSoukkachang@yahoo.com>
2019-04-26 00:53:22 -0500

    Added Todo and TodoList.

--- todo/src/App.js
+++ todo/src/App.js
@@ -6,7 +6,12 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <form>
+          <input placeholder="Enter a Task"></input>
+          <button>Add Task</button>
+        </form>
+
+        <div className="todoList"></div>
       </div>
     )
   }
